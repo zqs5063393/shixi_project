6f66ffb5cc249904a21b0822b6ade8570afa28fe
zqs5063393 <5063393@qq.com>
2023-12-21 19:20:22 +0800

    zhangqianshuaiV2

--- V1.m
+++ V1.m
@@ -1,3 +1,5 @@
+%该模块已在zhangqianshuaiV2版本中进行了修改，修改日期为2023-12-21 19:17
+
 all_msg=a_msg; %所有可能的信号
 all_msg_gf=gf(all_msg,3); %将所有的信号映射到GF域
 all_code_msg=rs_rscode(all_msg_gf) ; %对所有的信号进行RS编码，得到所有正确的编码
