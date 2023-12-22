bf0ac3779a3e3c1b8ecd03ff5f240a74e447edf2
yourcutedad <1521747115@qq.com>
2023-12-22 20:07:14 +0800

    denghaoyuanV2

--- rs_rscode.m
+++ rs_rscode.m
@@ -1,4 +1,4 @@
-%该模块在子分支zahngqianshuai_branch4中进行修改并已经合并到主分支zhangqianshuai_V4中
+%该模块在子分支zahngqianshuai_branch4中进行修改并已经合并到主分支zhangqianshuai_V4中！
 %RS码编码
 function code=rs_rscode(msg)
 g=[1 0 0 3 2 1 3
